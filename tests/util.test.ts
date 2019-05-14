8abf082f68378dd7ed868235eec6f40e57843144
wangshijun <wangshijun2010@gmail.com>
2019-05-14 16:22:49 +0800

    fix: handling scoped package names (#12)
    * fix: handling scoped package names
    
    * chore: remove only statement from test

---
+++ tests/util.test.ts
@@ -0,0 +1,16 @@
+import { formatName } from '../dist/util';
+
+describe('util.test.ts', () => {
+  describe('formatName', () => {
+    it('should format normal package names properly', () => {
+      expect(formatName('JS2DTS')).toEqual('JS2DTS');
+      expect(formatName('js2dts')).toEqual('js2dts');
+      expect(formatName('did-util')).toEqual('didUtil');
+    });
+
+    it('should format scoped package names properly', () => {
+      expect(formatName('@xmark/core')).toEqual('XmarkCore');
+      expect(formatName('@xmark/transform-landing-page')).toEqual('XmarkTransformLandingPage');
+    });
+  });
+});
